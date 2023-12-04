f17b7d90480c57a5f8b02c6b9655b7fc2dfa8300
Eduard <mecozma@gmail.com>
2023-12-04 21:22:24 +0200

    Added a FAQ page.

--- main.go
+++ main.go
@@ -11,7 +11,15 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:me@gmail.com\">me@gmail.com</a>.")
+	fmt.Fprint(w, `<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:me@gmail.com\">
+	me@gmail.com</a>.`)
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, `<h1>FAQ page</h1><h2>Is there a free version?</h2><p>Yes! We offer a free trial for 
+	30 days on any paid plans.</p><h2>What are your support hours?</h2><p> We have support staff 
+	answering emails 24/7, though response times may be a bit slower on weekends.</p><h2>How do I contact
+	support?</h2><p>Email us - <a href="support@lenslocked.com">support@lenslockes.com</a></p>`)
 }
 
 type Router struct{}
@@ -22,6 +30,8 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, "The server is a bit shy today, try again :)", http.StatusNotFound)
 	}
