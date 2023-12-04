6cdfbdc304bfa8d2fc27fd6f9ebcc1a003453180
Eduard <mecozma@gmail.com>
2023-12-04 19:16:16 +0200

    A different way of handling not found pages. Also, added a custom message.

--- main.go
+++ main.go
@@ -14,22 +14,16 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:me@gmail.com\">me@gmail.com</a>.")
 }
 
-func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-	http.NotFound(w, r)
-}
-
 // pathHandler function prints the path requested(typed in the search bar).
 func pathHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, r.URL.Path)
-	fmt.Fprintln(w, r.URL.RawPath)
-	// switch r.URL.Path {
-	// case "/":
-	// 	homeHandler(w, r)
-	// case "/contact":
-	// 	contactHandler(w, r)
-	// default:
-	// 	notFoundHandler(w, r)
-	// }
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, "The server is a bit shy, try again :)", http.StatusNotFound)
+	}
 }
 
 func main() {
