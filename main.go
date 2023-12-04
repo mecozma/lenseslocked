74d3fa45cdb1697c6b006f7d6ef3543170f2006c
Eduard <mecozma@gmail.com>
2023-12-04 20:16:40 +0200

    Added a custom router.

--- main.go
+++ main.go
@@ -14,21 +14,21 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:me@gmail.com\">me@gmail.com</a>.")
 }
 
-// pathHandler function prints the path requested(typed in the search bar).
-func pathHandler(w http.ResponseWriter, r *http.Request) {
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
 	default:
-		http.Error(w, "The server is a bit shy, try again :)", http.StatusNotFound)
+		http.Error(w, "The server is a bit shy today, try again :)", http.StatusNotFound)
 	}
 }
 
 func main() {
-	http.HandleFunc("/", pathHandler)
-	// http.HandleFunc("/contact", contactHandler)
+	var router Router
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 }
