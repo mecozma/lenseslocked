590d3551cb4877913a78289b41d6a2a6a6655d64
Eduard <mecozma@gmail.com>
2023-12-04 16:33:59 +0200

    Added the pathHandler function to examine the http.Request type.

--- main.go
+++ main.go
@@ -14,9 +14,14 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:me@gmail.com\">me@gmail.com</a>.")
 }
 
+// pathHandler function prints the path requested(typed in the search bar).
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, r.URL.Path)
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
+	// http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
