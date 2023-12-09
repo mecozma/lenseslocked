f487cc60b81b4ffa4dc06549270091b8cac2c74d
Eduard <mecozma@gmail.com>
2023-12-09 17:33:12 +0200

    Refactored the code to reflect the DRY principle, commented the code and updated the contact page to use a html template.

--- main.go
+++ main.go
@@ -11,11 +11,11 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+// executeTemplate function renders the html templates.
+func executeTemplate(w http.ResponseWriter, filepath string) {
 	w.Header().Set("Content-type", "text/html; charset=utf-8")
 	// tplPath joines the path before passing it to Parsefiles method to ensure it is working on any OS.
-	tplPath := filepath.Join("templates", "home.gohtml")
-	tpl, err := template.ParseFiles(tplPath)
+	tpl, err := template.ParseFiles(filepath)
 	if err != nil {
 		log.Printf("Parsing template: %v", err)
 		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
@@ -23,25 +23,36 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	err = tpl.Execute(w, nil)
 	if err != nil {
-		log.Printf("Executing template: %v\n", err)
+		log.Printf("Executing template: %v", err)
 		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
 		return
 	}
 }
 
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, `<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:me@gmail.com\">
-	me@gmail.com</a>.`)
+// homeHandler function handles the home page.
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tplPath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, tplPath)
 }
 
+// contactHandler function handles the contact page.
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	tplPath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tplPath)
+}
+
+// faqHandler function handles the faq page.
 func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
 	fmt.Fprint(w, `<h1>FAQ page</h1><h2>Is there a free version?</h2><p>Yes! We offer a free trial for 
 	30 days on any paid plans.</p><h2>What are your support hours?</h2><p> We have support staff 
 	answering emails 24/7, though response times may be a bit slower on weekends.</p><h2>How do I contact
 	support?</h2><p>Email us - <a href="support@lenslocked.com">support@lenslockes.com</a></p>`)
 }
 
+// galleryHandler function handles the gallery page.
 func galleryHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
 	userName := chi.URLParam(r, "userName")
 	w.Write([]byte(fmt.Sprintf("Hello %s!", userName)))
 }
