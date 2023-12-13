6e28c57726e2c537326046d6037fd43dd6758079
Eduard <mecozma@gmail.com>
2023-12-13 20:28:38 +0200

    Embedded the html templates and added support for parsing the embedded html templates.

--- main.go
+++ main.go
@@ -3,26 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/mecozma/lenslocked/controllers"
+	"github.com/mecozma/lenslocked/templates"
 	"github.com/mecozma/lenslocked/views"
 )
 
 func main() {
 	r := chi.NewRouter()
 
-	tpl := views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))
+	tpl := views.Must(views.ParseFS(templates.FS, "home.gohtml"))
 	r.Get("/", controllers.StaticHandler(tpl))
 
-	tpl = views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))
+	tpl = views.Must(views.ParseFS(templates.FS, "contact.gohtml"))
 	r.Get("/contact", controllers.StaticHandler(tpl))
 
-	tpl = views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))
+	tpl = views.Must(views.ParseFS(templates.FS, "faq.gohtml"))
 	r.Get("/faq", controllers.StaticHandler(tpl))
 
-	tpl = views.Must(views.Parse((filepath.Join("templates", "gallery.gohtml"))))
+	tpl = views.Must(views.ParseFS(templates.FS, "gallery.gohtml"))
 	r.Get("/gallery", controllers.StaticHandler(tpl))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
--- views/template.go
+++ views/template.go
@@ -3,6 +3,7 @@ package views
 import (
 	"fmt"
 	"html/template"
+	"io/fs"
 	"log"
 	"net/http"
 )
@@ -14,12 +15,11 @@ func Must(t Template, err error) Template {
 	return t
 }
 
-// Parse function parses the html templates.
-func Parse(filepath string) (Template, error) {
-	// tplPath joines the path before passing it to Parsefiles method to ensure it is working on any OS.
-	tpl, err := template.ParseFiles(filepath)
+// ParseFS function parses the html templates.
+func ParseFS(fs fs.FS, pattern string) (Template, error) {
+	tpl, err := template.ParseFS(fs, pattern)
 	if err != nil {
-		return Template{}, fmt.Errorf("parsing template: %w", err)
+		return Template{}, fmt.Errorf("parseFS template: %w", err)
 	}
 	t := Template{
 		htmlTpl: tpl,
