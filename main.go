fc05e779ca93b12c5b01aabdebf1726e41dc56dc
Eduard <mecozma@gmail.com>
2023-12-14 11:52:34 +0200

    Reusable templates using template parts.

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 func main() {
 	r := chi.NewRouter()
 
-	tpl := views.Must(views.ParseFS(templates.FS, "home.gohtml"))
+	tpl := views.Must(views.ParseFS(templates.FS, "home.gohtml", "layout-parts.gohtml"))
 	r.Get("/", controllers.StaticHandler(tpl))
 
 	tpl = views.Must(views.ParseFS(templates.FS, "contact.gohtml"))
